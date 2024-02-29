335258e68803c90ccbc9c839188a8ca58a2d704a
Ramvagole <ramvagole@gmail.com>
2024-02-29 11:09:17 +0530

    new js intro to cp

--- src/index.js
+++ src/index.js
@@ -41,3 +41,21 @@ function objectFactory(fname, age) {
 }
 
 export { add, greet, interpolate, nuller, objectFactory };
+let a=add(1,2);
+console.log(a);
+
+let b=greet();
+console.log(b);
+
+let c=interpolate(2,3);
+let d=interpolate(5,2);
+console.log(`interpolate value is ${c}`);
+console.log(`interpolate vlaue is ${d}`);
+
+let nullValue=null;
+nuller(nullValue);
+
+let e=objectFactory("Raj",26);
+let f=objectFactory("Vijay",24);
+console.log(e);
+console.log(f);
